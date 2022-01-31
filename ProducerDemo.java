9993486ba141ce3f42ecb16c3e541193517ce078
saurabhlambe <saurabh.lambe10@gmail.com>
2022-01-31 13:34:46 +0530

    added some more code

---
+++ ProducerDemo.java
@@ -0,0 +1,39 @@
+package com.github.simple;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+
+        String bootstrapServers="127.0.0.1:9092";
+
+        // Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create Producer
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create a producer record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>("first_topic", "Henlo");
+
+        // send data
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush and close program
+        producer.close();
+
+    }
+}
